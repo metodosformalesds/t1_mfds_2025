f0f12cbad3b766b488aa3c3eae48b04f158d8501
GabrielVilchis-215460 <al215460@alumnos.uacj.mx>
2025-11-08 02:29:42 -0700

    Main.py fixed

--- Backend/app/main.py
+++ Backend/app/main.py
@@ -1,45 +1,8 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from app.config import settings
-from app import models
-from app.api.v1.auth import router as auth_router
-from app.core.database import engine 
 
-models.Base.metadata.create_all(bind=engine) # Crea las tablas de la base de datos
-
-app = FastAPI(
-    #title="BeFit",
-    description="API Backend para BeFit - E-commerce de suplementos y fitness",
-    version="1.0.0",
-    docs_url="/docs",
-    redoc_url="/redoc"
-)
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-)
+app = FastAPI()
 
 # Esto es una prueba para probar el comando de uvicorn
 @app.get("/")
 def root():
     return {"message": "Welcome to the T1-MFDS 2025 Backend!"}
-
-@app.get("/", tags=["Health"])
-def read_root():
-    return {
-        "message": f"está funcionando correctamente",
-        "version": "1.0.0",
-        "status": "healthy"
-    }
-
-@app.get("/health", tags=["Health"])
-def health_check():
-    return {
-        "status": "ok",
-        "database": "connected", 
-        "s3": "configured"
-    }
-
-# Rutas del modulo de auth/cognito
-app.include_router(auth_router)
