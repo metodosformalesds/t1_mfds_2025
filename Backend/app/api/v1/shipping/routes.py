7ae37a5d2513743859fcb346729e95e247c220f7
GabrielVilchis-215460 <al215460@alumnos.uacj.mx>
2025-11-12 02:15:26 -0700

    Shipping routes added

---
+++ Backend/app/api/v1/shipping/routes.py
@@ -0,0 +1,40 @@
+from app.core.database import get_db
+from app.api.v1.shipping.schemas import Order, CreateOrder, OrderTrackingResponse
+from app.api.v1.shipping import service
+from sqlalchemy.orm import Session
+from fastapi import APIRouter, BackgroundTasks, HTTPException, status, Depends
+
+router = APIRouter(prefix="/shipping", tags=["Shipping"])
+
+"""
+Esta funcion crea un pedido con sus items
+"""
+@router.post("/crear-pedido/", response_model=Order, status_code=status.HTTP_201_CREATED)
+def crear_pedido(pedido_in: CreateOrder, db: Session = Depends(get_db)):
+    
+    try:
+        nuevo_pedido = service.create_order_db(db=db, order_in=pedido_in)
+    except HTTPException as http_exc:
+        raise http_exc
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e)
+        )
+    return nuevo_pedido
+
+
+"""
+Esta funcion consulta el estado del envio, tracking, y lista de productos    
+"""
+@router.get("/rastrear-pedido/{pedido_id}", response_model=OrderTrackingResponse)
+def rastrear_pedido( pedido_id: int, db: Session = Depends(get_db)):
+    tracking_details = service.get_details(db=db, pedido_id=pedido_id)
+    
+    if not tracking_details:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND, 
+            detail="Pedido no encontrado"
+        )
+        
+    return tracking_details
