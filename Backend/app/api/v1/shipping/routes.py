f7ec61bdc50843a426665e9e369b26ed051fb908
GabrielVilchis-215460 <al215460@alumnos.uacj.mx>
2025-11-12 02:19:14 -0700

    Refactor shipping routes to use explicit ShippingService import

--- Backend/app/api/v1/shipping/routes.py
+++ Backend/app/api/v1/shipping/routes.py
@@ -1,8 +1,8 @@
 from app.core.database import get_db
 from app.api.v1.shipping.schemas import Order, CreateOrder, OrderTrackingResponse
-from app.api.v1.shipping import service
+from app.api.v1.shipping.service import ShippingService
 from sqlalchemy.orm import Session
-from fastapi import APIRouter, BackgroundTasks, HTTPException, status, Depends
+from fastapi import APIRouter, HTTPException, status, Depends
 
 router = APIRouter(prefix="/shipping", tags=["Shipping"])
 
@@ -13,7 +13,7 @@ Esta funcion crea un pedido con sus items
 def crear_pedido(pedido_in: CreateOrder, db: Session = Depends(get_db)):
     
     try:
-        nuevo_pedido = service.create_order_db(db=db, order_in=pedido_in)
+        nuevo_pedido = ShippingService.create_order_db(db=db, order_in=pedido_in)
     except HTTPException as http_exc:
         raise http_exc
     except Exception as e:
@@ -29,7 +29,7 @@ Esta funcion consulta el estado del envio, tracking, y lista de productos
 """
 @router.get("/rastrear-pedido/{pedido_id}", response_model=OrderTrackingResponse)
 def rastrear_pedido( pedido_id: int, db: Session = Depends(get_db)):
-    tracking_details = service.get_details(db=db, pedido_id=pedido_id)
+    tracking_details = ShippingService.get_details(db=db, pedido_id=pedido_id)
     
     if not tracking_details:
         raise HTTPException(
